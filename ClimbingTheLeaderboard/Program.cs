5bf6b5858fe0fd94b2603a69583c0d91da248459
Haidar Amir Faruqi <intern.faruqi@m3.formulatrix.com>
2023-10-17 11:54:17 +0700

    First Commit

---
+++ ClimbingTheLeaderboard/Program.cs
@@ -0,0 +1,60 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.Write("Input number of players in the leaderboard: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Input leaderboard scores: ");
+        List<int> ranked = Array.ConvertAll(Console.ReadLine()!.Split(" "), Convert.ToInt32).ToList();
+        
+        Console.Write("Input number of games the player plays: ");
+        int m = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Input player's game scores: ");
+        List<int> player = Array.ConvertAll(Console.ReadLine()!.Split(" "), Convert.ToInt32).ToList();
+
+        List<int> ranks = ClimbingTheLeaderboard(ranked, player);
+
+        foreach (var rank in ranks)
+        {
+            Console.WriteLine("Rank: " + rank);
+        }
+    }
+
+    private static List<int> ClimbingTheLeaderboard(List<int> ranked, List<int> player)
+    {
+        var ranks = new int[player.Count];
+
+        int index = player.Count - 1;
+        int scoreNow = 0;
+        int rankNow = 0;
+        foreach (var rankedScore in ranked)
+        {
+            if (rankedScore != scoreNow)
+            {
+                scoreNow = rankedScore;
+                rankNow++;
+            }
+
+            if (player[index] >= rankedScore)
+            {
+                ranks.SetValue(rankNow, index);
+                index--;
+            }
+            if (player[index] < rankedScore)
+            {
+                rankNow++;
+                ranks.SetValue(rankNow, index);
+                index--;
+            }
+
+            if (index < 0)
+            {
+                break;
+            }
+        }
+
+        return ranks.ToList();
+    }
+}
---
+++ Mock2/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        var matrix = new List<List<int>>() { new List<int>() { 1, 2 }, new List<int>() { 3, 4 } };
+        int result = FlippingMatrix(matrix);
+        Console.WriteLine("Result: " + result);
+    }
+    
+    public static int FlippingMatrix(List<List<int>> matrix)
+    {
+        Console.WriteLine("List count: " + matrix.Count);
+        int n = matrix.Count / 2;
+        int result = 0;
+        for (int i = 0; i < matrix.Count; i++)
+        {
+            for (int j = 0; j < matrix.Count; j++)
+            {
+                var temp = new List<int>
+                {
+                    matrix[i][j],
+                    matrix[i][2 * n - j - 1],
+                    matrix[2 * n - i - 1][j],
+                    matrix[2 * n - i - 1][2 * n - j - 1]
+                };
+                result += temp.Max();
+            }
+        }
+
+        return result;
+    }
+}
